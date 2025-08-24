58edff4c225b67c322d69308a601aba4c528d5f8
Saltos150 <sindicatolibrevalencia@gmail.com>
2025-08-24 19:33:12 +0200

    Update App.js

--- App.js
+++ App.js
@@ -341,7 +341,7 @@ function App() {
         <div className="h-screen overflow-hidden bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-800 dark:to-gray-950 text-gray-900 dark:text-gray-100 font-inter p-4 sm:p-6 md:p-8">
             <div className="max-w-4xl mx-auto bg-white dark:bg-gray-800 rounded-xl shadow-2xl p-6 sm:p-8 md:p-10 h-full flex flex-col">
                 <div className="flex-shrink-0 mb-8">
-                    <h1 className="text-4xl font-extrabold text-center text-gray-700 dark:text-gray-300 mb-6">📍 Gestor de Visitas S. LIBRE Valencia</h1>
+                    <h1 className="text-4xl font-extrabold text-center text-gray-700 dark:text-gray-300 mb-6">📍 Gestor de Visitas SINDICATO LIBRE Valencia</h1>
                     <div className="relative">
                         <span className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
